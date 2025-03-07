6298a0b45fed7f7bfde4963ed342720323536b6f
avramandreea <andreeai.avram@ulbsibiu.ro>
2025-03-07 09:36:35 +0200

    Ingineria sistemelor de programe

---
+++ src/ro/ulbs/paradigme/lab2/Circle.java
@@ -0,0 +1,26 @@
+package ro.ulbs.paradigme.lab2;
+
+public class Circle extends Form {
+    private float radius;
+
+    public Circle() {
+        super();
+        this.radius = 0;
+    }
+
+    public Circle(String color, float radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    @Override
+    public float getArea() {
+        return (float) (Math.PI * radius * radius);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Circle with radius " + radius;
+    }
+}
+
---
+++ src/ro/ulbs/paradigme/lab2/Triangle.java
@@ -0,0 +1,28 @@
+package ro.ulbs.paradigme.lab2;
+
+public class Triangle extends Form {
+    private float height;
+    private float base;
+
+    public Triangle() {
+        super();
+        this.height = 0;
+        this.base = 0;
+    }
+
+    public Triangle(float height, float base,String color) {
+        super(color);
+        this.height = height;
+        this.base = base;
+    }
+
+    @Override
+    public float getArea() {
+        return (base * height) / 2;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Triangle with base " + base + " and height " + height;
+    }
+}
