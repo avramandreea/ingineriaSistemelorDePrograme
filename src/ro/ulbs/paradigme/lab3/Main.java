95d87ae96d4d1e6301c6b546b3339ccb95a2a192
avramandreea <andreeai.avram@ulbsibiu.ro>
2025-03-14 11:04:26 +0200

    Ingineria sistemelor de programe laborator 3

--- src/ro/ulbs/paradigme/lab3/Main.java
+++ src/ro/ulbs/paradigme/lab3/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        PasswordMaker1 pm1 = PasswordMaker1.getInstance();
+        PasswordMaker1 pm1 = new PasswordMaker1();
         PasswordMaker1 pm2 = PasswordMaker1.getInstance();
         System.out.println("Generated Password: " + pm1.getPassword());
         System.out.println("Access Counter: " + PasswordMaker1.getAccessCounter());
